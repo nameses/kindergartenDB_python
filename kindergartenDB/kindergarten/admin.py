11c6dd446c86d6b3599c745d1bf9ea5a3cec8bf2
okf0k <lubomvis@gmail.com>
2022-12-10 16:59:08 +0200

    create supper user, register admin panel

--- kindergartenDB/kindergarten/admin.py
+++ kindergartenDB/kindergarten/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+admin.site.register(models.Kindergarten)
+admin.site.register(models.KindergartenGroup)
+admin.site.register(models.Child)
+admin.site.register(models.Parent)
+admin.site.register(models.Family)
+admin.site.register(models.Month)
+admin.site.register(models.Attendance)
