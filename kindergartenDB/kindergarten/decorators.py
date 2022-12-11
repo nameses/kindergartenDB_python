1cdf80b589c555aa0601897cd022d2d096a693e8
okf0k <lubomvis@gmail.com>
2022-12-11 21:28:24 +0200

    feat: authenticated_only and post_method_only decorators

---
+++ kindergartenDB/kindergarten/decorators.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponseRedirect, Http404
+
+
+def authenticated_only(func):
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return func(request, *args, **kwargs)
+        return HttpResponseRedirect('/signup')
+
+    return wrapper
+
+
+def post_method_only(func):
+    def wrapper(request, *args, **kwargs):
+        if request.method == 'POST':
+            return func(request, *args, **kwargs)
+        raise Http404()
+
+    return wrapper
