09bde54a79b6a16d586d066a75268a4f2608bbb6
okf0k <lubomvis@gmail.com>
2022-12-11 21:04:26 +0200

    creating plain index view

--- kindergartenDB/kindergartenDB/urls.py
+++ kindergartenDB/kindergartenDB/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('shop.urls')),
+    path('', include('kindergarten.urls')),
     path('admin/', admin.site.urls),
 ]
