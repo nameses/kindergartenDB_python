972e72f85b90278a54fb19f901522cc0c134c3f8
okf0k <lubomvis@gmail.com>
2022-12-11 21:41:02 +0200

    wip: create auth forms

---
+++ kindergartenDB/kindergarten/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class SignUpForm(forms.Form):
+    username = forms.CharField()
+    surname = forms.CharField()
+    name = forms.CharField()
+    patronymic = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput)
