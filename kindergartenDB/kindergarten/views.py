a6b8470af34f1b10e5e410ccd6aff364affb4c3d
okf0k <lubomvis@gmail.com>
2022-12-12 14:39:11 +0200

    wip: kindergarten list

--- kindergartenDB/kindergarten/views.py
+++ kindergartenDB/kindergarten/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+from kindergartenDB.authorization.decorators import staff_only
+from kindergartenDB.kindergarten.models import Kindergarten
+
 
 def index(request):
     return render(request, 'kindergarten/index.html')
+
+
+@staff_only
+def kindergarten_list(request):
+    return render(
+        request,
+        'kindergarten/kindergarten_list.html',
+        {
+            'kindergartens': Kindergarten.objects.all()
+        }
+    )
