574150b4535c312f41120b085e02dd3b827e9e96
okf0k <lubomvis@gmail.com>
2022-12-16 14:06:03 +0200

    register UAI in admin panel

--- kindergartenDB/kindergarten/admin.py
+++ kindergartenDB/kindergarten/admin.py
@@ -4,6 +4,7 @@ from . import models
 
 # Register your models here.
 
+admin.site.register(models.UserAdditionInfo)
 admin.site.register(models.Kindergarten)
 admin.site.register(models.KindergartenGroup)
 admin.site.register(models.Child)
